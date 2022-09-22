7b90fba2dd0fbd14fcedc457b448f81bfde98a00
ArtiomOganesyan <artiomoganesyan@gmail.com>
2022-09-22 17:35:39 +0300

    done func

---
+++ func.js
@@ -0,0 +1,3 @@
+module.exports = {
+    func: () => console.log("func")
+}
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
+const {func} = require('./func');
 const main = () => {
+    func()
     console.log("main")
+    func()
 }
 
 console.log("hello")
